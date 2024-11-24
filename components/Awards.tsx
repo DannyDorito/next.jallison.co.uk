ac07f207a02a4109e3fc7765811d435ebdbb4b94
John Allison <john@jallison.co.uk>
2024-11-24 01:49:42 +0000

    Update Awards.tsx

--- components/Awards.tsx
+++ components/Awards.tsx
@@ -1,6 +1,6 @@
 import { awards } from "@/data/AwardsData";
 
-const Awards = () => {
+export const Awards = () => {
   return (
     <div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
