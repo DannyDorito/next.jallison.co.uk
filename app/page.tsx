742820ddbd38090de7088dddf0553e2a478977cf
John Allison <john@jallison.co.uk>
2025-04-28 23:05:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,11 @@ import { About } from "@/components/about";
 import { Contact } from "@/components/contact";
 import { Footer } from "@/components/footer";
 import { Home } from "@/components/home";
-import { Navigation } from "@/components/navigation";
 import { Projects } from "@/components/projects";
 
 export default function Page() {
   return (
     <>
-      <Navigation />
       <Home />
       <About />
       <Projects />
